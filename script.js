18239650b9b2b96fae3c5865584326043f1a21d0
“yadityo” <21552011140@sttbandung.ac.id>
2025-09-09 22:29:14 +0700

    delete index.html style.css and script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("downloadBtn").addEventListener("click", () => {
-  const element = document.querySelector(".proposal-container");
-  html2pdf().from(element).save("Proposal.pdf");
-});
